91f63350759ef8ccb8d91e7b45c351c2d5f30051
Harsimran <harsimran.kaur@tothenew.com>
2021-03-07 22:34:56 +0530

    changes uploaded

--- app/src/main/java/com/example/multipleview/MainActivity.kt
+++ app/src/main/java/com/example/multipleview/MainActivity.kt
@@ -3,100 +3,115 @@ package com.example.multipleview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private val mylist = mutableListOf<Int>()
-    private var page =1
-    private  var isLoading = false
-    private val limit= 4
+    private val listViewType2 = mutableListOf<Int>()
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var myAdapter: MyAdapter
+    private var startPage = 1
+    private var isLoading = false
+    private val limit = 4
+
+    private var progressBar : ProgressBar? = null
+
+    private lateinit var recyclerView2: RecyclerView
+    private lateinit var multiViewRecyclerAdapter: MyAdapter
     private lateinit var layoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         recyclerView = findViewById(R.id.recyclerView)
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
 
-        layoutManager  = LinearLayoutManager(applicationContext)
-        recyclerView.layoutManager = layoutManager
 
+        recyclerView2 = findViewById(R.id.recyclerView)
+
+        layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView2.layoutManager = layoutManager
+
+        multiViewRecyclerAdapter = MyAdapter(this, listViewType2)
+        recyclerView2.adapter = multiViewRecyclerAdapter
+
+        //getPage()
         getPage()
+        //checkForPagination()
+        Handler().post({checkForPagination()})
 
+        recyclerView2.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView2: RecyclerView, dx: Int, dy: Int) {
 
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if(dy>0){
-                    val visibleItemCount = layoutManager.childCount
-                    val pastVisibleItem = layoutManager.findFirstCompletelyVisibleItemPosition()
-                    val total=layoutManager.itemCount
-
-                    if(!isLoading)
-                    {
-                        if((visibleItemCount + pastVisibleItem) >= total)
-                            page++
-                        getPage()
-                    }
+                // dy means vertical scroll position
+                if (dy > 0) {
+                    Log.e("value of dy" , "dy " + dy )
+                    checkForPagination()
                 }
-
-                super.onScrolled(recyclerView, dx, dy)
+                super.onScrolled(recyclerView2, dx, dy)
             }
         })
     }
 
-    fun getPage(){
-        isLoading = true
+    private fun getPage() {
+
+        Log.e("get Page TAG", startPage.toString())
+        // When loading
+        // show progressBar
 
 
-        var progressBar =  findViewById<ProgressBar>(R.id.progressBar)
-        progressBar.visibility= View.VISIBLE
+        // For endless scrolling
+        addDataInList()
+        // Adding delay of 2 seconds
 
-        val start = (page-1)*limit
-        val end = page*limit
+        progressBar!!.visibility = View.GONE
 
-        for(i in start .. end){
+        multiViewRecyclerAdapter.notifyDataSetChanged()
+        isLoading = false
 
-            when{
-                i%2==0->{
-                    mylist.add(MyAdapter.TEXT_VIEW)
+
+    }
+    private fun checkForPagination(){
+        val visibleItemCount = layoutManager.childCount
+        val totalItemCount = layoutManager.itemCount
+        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+        // use isLastPage to stop when last page shown
+        // to the users
+        Log.e("Scrolled View variables", "visibleItemCount" + visibleItemCount)
+        Log.e("Scrolled View variables", "totalItemCount" + totalItemCount)
+        Log.e("Scrolled View variables", "firstVisibleItemPosition" + firstVisibleItemPosition)
+        if (!isLoading) {
+            if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount) {
+                startPage++
+                progressBar!!.visibility = View.VISIBLE
+                isLoading = true
+                Handler().postDelayed({getPage()}, 2000)
+            }
+        }
+    }
+
+
+    private fun addDataInList(){
+        val start = (startPage - 1) * limit
+        val end = (startPage) * limit
+        //val start = 1
+        //val end = 5
+
+        for (i in start..end) {
+            when {
+                i % 2 == 0 -> {
+                    listViewType2.add(MyAdapter.TEXT_VIEW)
                 }
-                i%3==0->{
-                    mylist.add(MyAdapter.IMAGE_VIEW)
+                i % 3 == 0 -> {
+                    listViewType2.add(MyAdapter.IMAGE_VIEW)
                 }
-                else->{
-                    mylist.add(MyAdapter.IMAGE_TEXT_VIEW)
+                else -> {
+                    listViewType2.add(MyAdapter.IMAGE_TEXT_VIEW)
                 }
             }
         }
-        Handler().postDelayed({
-            if (this::myAdapter.isInitialized){
-                myAdapter.notifyDataSetChanged()
-            }
-            else{
-                myAdapter= MyAdapter(this,mylist)
-                recyclerView.adapter = myAdapter
-            }
-            isLoading = false
-            progressBar.visibility= View.GONE
-
-        },3000)
-
-//        val listViewType = mutableListOf( MyAdapter.TEXT_VIEW,
-//        MyAdapter.IMAGE_VIEW,
-//        MyAdapter.IMAGE_TEXT_VIEW,
-//            MyAdapter.IMAGE_VIEW,
-//            MyAdapter.IMAGE_TEXT_VIEW,
-//        MyAdapter.IMAGE_VIEW,
-//        MyAdapter.IMAGE_TEXT_VIEW)
-
-
-//        val adapterRecyclerView = MyAdapter(this, listViewType)
-//        recyclerView.adapter=adapterRecyclerView
-
     }
 }
